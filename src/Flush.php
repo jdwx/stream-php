f611344959af56613f3ea8e4cfff8c7207973393
JDW <xs@jdw.sx>
2025-11-27 15:08:16 -0500

    Import Flush class from JDWX\Web.

---
+++ src/Flush.php
@@ -0,0 +1,18 @@
+<?php
+
+
+declare( strict_types = 1 );
+
+
+namespace JDWX\Stream;
+
+
+class Flush implements \Stringable {
+
+
+    public function __toString() : string {
+        return '';
+    }
+
+
+}
